ebdf75bddc20a1d6110f0cc87f6c61af42eab3d7
Gyvastis <shibalba1@gmail.com>
2021-04-12 18:47:42 +0200

    add dummy test

---
+++ tests/DummyTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PhpArsenal\SalesforceBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testTrue(): void {
+        $this->markAsRisky();
+    }
+}
