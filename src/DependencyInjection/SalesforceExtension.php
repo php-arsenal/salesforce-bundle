bb221ce5d2845242f18e473e25898a2e141431bd
Gyvastis <shibalba1@gmail.com>
2021-04-13 12:13:18 +0200

    update config

--- src/DependencyInjection/SalesforceExtension.php
+++ src/DependencyInjection/SalesforceExtension.php
@@ -26,11 +26,11 @@ class SalesforceExtension extends Extension
 
         $loader->load('soap_client.xml');
         $loader->load('rest_client.xml');
-        foreach ($processedConfiguration['soap_client'] as $key => $value) {
+        foreach ($processedConfiguration as $key => $value) {
             $container->setParameter('salesforce.soap_client.' . $key, $value);
         }
 
-        if (true == $processedConfiguration['soap_client']['logging']) {
+        if (true == $processedConfiguration['logging']) {
             $builder = $container->getDefinition('salesforce.soap_client.builder');
 //            $builder->addMethodCall('withLog', array(new Reference('salesforce.logger')));
         }
